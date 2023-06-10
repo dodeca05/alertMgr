5bcfe1a9f065714eec88d403c73391c7aaa661ed
jaehyekim <jaehyekim@ebaykorea.com>
2023-06-10 23:17:57 +0900

    Initial commit

---
+++ src/main/java/com/alertmgr/AlertMgrApplication.java
@@ -0,0 +1,13 @@
+package com.alertmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertMgrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertMgrApplication.class, args);
+    }
+
+}
