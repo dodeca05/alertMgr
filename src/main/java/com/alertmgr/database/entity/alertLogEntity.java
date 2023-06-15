240e4f9cf315c452df20713ef1db7d0e099d3983
jaehyekim <jaehyekim@ebaykorea.com>
2023-06-16 00:09:54 +0900

    기본 서비스 생성 주석 추가

--- src/main/java/com/alertmgr/database/entity/alertLogEntity.java
+++ src/main/java/com/alertmgr/database/entity/alertLogEntity.java
@@ -3,8 +3,9 @@ package com.alertmgr.database.entity;
 import java.util.Date;
 
 public class alertLogEntity {
-    public String alertServiceName;
-    public String target;
-    public String msg;
-    public Date date;
+    public String alertServiceName;//어떤 방식으로 알림을 보냈는지
+    public String type;//에러 또는 정보
+    public String target;//누구에게 보냈는지 all 또는 전부
+    public String msg;//메세지
+    public Date date;//생성 날짜
 }
